7961321a5a8fd98f33aa1159a9bede8bab564a2e
pratik822 <pratik822@gmail.com>
2025-01-01 20:23:16 +0530

    Add files via upload

--- WishListCompose.kt
+++ WishListCompose.kt
@@ -6,11 +6,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
@@ -20,7 +23,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
@@ -28,10 +35,16 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.pratik.loginmodule.UIState
 import com.pratik.loginmodule.cleanArch.dao.WishListDTO
 import com.pratik.loginmodule.cleanArch.dao.ui.theme.LoginModuleTheme
+import com.pratik.loginmodule.cleanArch.viewmodels.WishlistViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class WishListCompose : ComponentActivity() {
+
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +55,7 @@ class WishListCompose : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    Greeting(name = "")
 
                 }
             }
@@ -50,7 +64,7 @@ class WishListCompose : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
+fun Greeting(name: String, wishlistViewModel: WishlistViewModel = hiltViewModel()) {
     val wishListDTO = WishListDTO(
         availabilityStatus = "status",
         brand = "brand",
@@ -60,17 +74,49 @@ fun Greeting(name: String) {
         rating = 3.5,
         title = "title", description = "description"
     )
-    WishListCard(
-        product = wishListDTO,
-        productImageRes = R.drawable.ic_menu_search,
-        onAddToBagClick = {
-            println("Item added to bag!")
-        }, onDeleteClick = {
+    LaunchedEffect(Unit) {
+        wishlistViewModel.getAllWishlist()
 
-        }, onItemClick = {
+    }
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val uiState by wishlistViewModel.wishListStateFlow.collectAsState()
+        when (uiState) {
+            is UIState.Loading -> {
+                //  CircularProgressIndicator()
 
+            }
+
+            is UIState.Success -> {
+                var result = (uiState as UIState.Success<List<WishListDTO>>).data
+                LazyColumn(contentPadding = PaddingValues(8.dp)) {
+                    items(result) { item ->
+                        WishListCard(
+                            product = item,
+                            productImageRes = R.drawable.ic_menu_search,
+                            onAddToBagClick = { /*TODO*/ },
+                            onDeleteClick = {
+                                wishlistViewModel.deleteWishList(item.id)
+                            },
+                            onItemClick = { /*TODO*/ })
+                    }
+
+                }
+            }
+
+            is UIState.Error -> {
+                Text(
+                    "Error: ${(uiState as UIState.Error).message}",
+                    modifier = Modifier.fillMaxSize()
+                )
+
+            }
         }
-    )
+    }
+
+
 }
 
 
